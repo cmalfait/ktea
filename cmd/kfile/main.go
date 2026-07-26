4412a894465acfbf1a84152fc413ed948400c54f
cmalfait <chad.malfait@oracle.com>
2026-07-26 08:24:53 -0600

    cleanup

--- cmd/kfile/main.go
+++ cmd/kfile/main.go
@@ -7,11 +7,8 @@ import (
 )
 
 func main() {
-	var strFlag string
-
-	flag.StringVar(&strFlag, "c", "link", "env|link ('env' sets KUBECONFIG|'link' creates link)")
+	strFlagPtr := flag.String("type", "link", "env|link ('env' sets KUBECONFIG|'link' creates link)")
 	flag.Parse()
 
-	kfile.Kfile(strFlag, "/home/cmalfait/.kube")
-	//kfile.Kfile("env", "/home/cmalfait/.kube")
+	kfile.Kfile(*strFlagPtr, "/home/cmalfait/.kube")
 }
